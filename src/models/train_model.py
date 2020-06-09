4cfc6b5a8be5f514de0ad60bc2bbc2dc90fb5675
MauiGonzo <maurice.verbrugge@gmail.com>
2020-06-09 18:50:22 +0200

    corrected import and arg

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,10 @@
-import src.models.model
 from src.models.model import U_net_3D_model
 import tensorflow as tf
 
 
 # fit model
 def train_u_net(train_gen, val_gen, epochs):
-    weight_path = 'models/temp/model_{epoch:02d}-{val_loss:.2f}.h5' # TODO: needs change
+    weight_path = 'models/temp/model_{epoch:02d}-{val_loss:.2f}.h5'  # TODO: needs change
     optimizer = tf.keras.optimizers.Adam(lr=1E-3, beta_1=0.9, beta_2=0.999, epsilon=1E-08)
     u_model = U_net_3D_model()
     # TODO: optional use keras model loader
@@ -14,9 +13,9 @@ def train_u_net(train_gen, val_gen, epochs):
     # compile model
     # loss_fn = tf.keras.losses.mean_squared_error(y_pred=None, y_pred=None)
     # TODO: make loss
-    u_model.compile(#loss=loss_fn,
-                  optimizer=optimizer)
-                  # metrics=YOUR METRIC
+    u_model.compile(  # loss=loss_fn,
+        optimizer=optimizer)
+    # metrics=YOUR METRIC
 
     # create callback list
     earlystopper = tf.keras.callbacks.EarlyStopping(patience=10, verbose=1)
@@ -28,7 +27,7 @@ def train_u_net(train_gen, val_gen, epochs):
     callback_list = [checkpoint, earlystopper, tensorboard]
 
     # TODO: make it possible to apply custom model name
-    results = model.fit(train_gen,
-                        epochs=epochs,
-                        vallideation_data=val_gen,
-                        callbacks=callback_list)
+    results = u_model.fit(train_gen,
+                          epochs=epochs,
+                          validation_data=val_gen,
+                          callbacks=callback_list)
