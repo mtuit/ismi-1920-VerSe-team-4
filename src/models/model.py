6e3db3619c6b0fb1adb21e2d0427c6a406ba9d2b
MauiGonzo <maurice.verbrugge@gmail.com>
2020-06-11 12:02:25 +0200

    first running-ish code

--- src/models/model.py
+++ src/models/model.py
@@ -2,15 +2,18 @@
 # input = image
 # output = x*y grid, each containing:
 # one centroid location with x,y,w,h and the class (1-25) with a certainty score of: c
-
 import tensorflow as tf
 
 
-def U_net_3D_model():
-    inputs = tf.keras.Input(shape=(None, 32, 64, 64))
+def U_net_3D_model(input_shape=(128, 64, 64)):
+    # def U_net_3D_model(input_shape=(None, 32, 64, 64)):
+
+    inputs = tf.keras.Input(shape=input_shape)
+    x = tf.expand_dims(inputs, -1)
+    # inputs = tf.keras.Input(shape=(None, 128, 64, 64))
 
     # block 1
-    x = tf.keras.layers.Conv3D(32, (3, 3, 3), padding="same")(inputs)
+    x = tf.keras.layers.Conv3D(32, (3, 3, 3), padding="same")(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.activations.relu(x, alpha=0.0, max_value=None, threshold=0)#(x)
 
@@ -22,7 +25,7 @@ def U_net_3D_model():
     x = tf.keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(s1)
 
     # block 2
-    x = tf.keras.layers.Conv3D(64, (3, 3, 3), padding="same")(inputs)
+    x = tf.keras.layers.Conv3D(64, (3, 3, 3), padding="same")(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.activations.relu(x, alpha=0.0, max_value=None, threshold=0)#(x)
 
@@ -34,7 +37,7 @@ def U_net_3D_model():
     x = tf.keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(s2)
 
     # block 3
-    x = tf.keras.layers.Conv3D(128, (3, 3, 3), padding="same")(inputs)
+    x = tf.keras.layers.Conv3D(128, (3, 3, 3), padding="same")(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.activations.relu(x, alpha=0.0, max_value=None, threshold=0)#(x)
 
@@ -67,7 +70,7 @@ def U_net_3D_model():
     x = tf.keras.activations.relu(x, alpha=0.0, max_value=None, threshold=0)#(x)
 
     # block 5
-    # x = tf.keras.layers.UpSampling3D(size=(2, 2, 2))(x)
+    x = tf.keras.layers.UpSampling3D(size=(2, 2, 2))(x)
     x = tf.keras.layers.concatenate([s2, x], axis=4)
 
     x = tf.keras.layers.Conv3D(128, (3, 3, 3), padding="same")(x)
@@ -79,7 +82,7 @@ def U_net_3D_model():
     x = tf.keras.activations.relu(x, alpha=0.0, max_value=None, threshold=0)#(x)
 
     # block 6
-    # x = tf.keras.layers.UpSampling3D(size=(2, 2, 2))(x)
+    x = tf.keras.layers.UpSampling3D(size=(2, 2, 2))(x)
     x = tf.keras.layers.concatenate([s1, x], axis=4) # was s2
 
     x = tf.keras.layers.Conv3D(64, (3, 3, 3), padding="same")(x)
@@ -95,6 +98,11 @@ def U_net_3D_model():
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     # TODO: add loss(cross entropy?) add metrics
-    model.summary()  # als dit kan?
+    model.summary()
+    # for layer in model.layers:
+    #     print('input shape: ')
+    #     print(layer.input_shape)
+    #     print('output shape: ')
+    #     print(layer.output_shape)
 
     return model
